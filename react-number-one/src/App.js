52fdda0a487fdec2b387d52831a148f4d510ef3f
Samuel Nunes <samstiifler@gmail.com>
2020-05-27 22:27:35 -0300

    Classe é funções

--- react-number-one/src/App.js
+++ react-number-one/src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Opa, Suava na moral???</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/*class App extends React.Component {
+  render() {
+    return <h1>Silp3r T3st</h1>;
+  }
+}*/
+
+/*function App() {
+  return <h1>Sam Silper Jay</h1>;
+}*/
+
+/*let App = () => {
+  return <h1>S4m S1p3l J4y</h1>;
+};*/
+
+let App = () => <h1>Sil na porr* do Beat</h1>;
 
 export default App;
