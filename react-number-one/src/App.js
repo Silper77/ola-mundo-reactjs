c14e6ed19c0233594e5a8c4628603379ecc194a4
Samuel Nunes <samstiifler@gmail.com>
2020-06-02 19:36:30 -0300

    add no react style sheets

--- react-number-one/src/App.js
+++ react-number-one/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
-import "./App.css";
 
 function Avatar(props) {
   return (
-    <div className="avatar">
-      <img src={props.user.url} alt={props.user.name} className="avatar img" />
+    <div style={{ backgroundColor: "red", padding: 20, width: 150 }}>
+      <img
+        style={{ width: 50, height: 50 }}
+        src={props.user.url}
+        alt={props.user.name}
+      />
       <br />
       <span>{props.user.name}</span>
     </div>
